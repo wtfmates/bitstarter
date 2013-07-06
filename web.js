f0b20ec67800dd07d34f8f252df86244fc3ed950
wtfmates <wtfmates@gmail.com>
2013-07-06 16:35:36 +0000

    hopefully the last

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var text = fs.readFileSync("index.html");
+  var text = fs.readFileSync("index.html",'utf8');
   response.send(text);
   response.end();
 });
